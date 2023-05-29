d4a42351d6bc2bc4d9aa2ebab9a492c83d0705b1
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-05-28 23:38:10 -0600

    Modified Script

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ else
   sudo apt update 
   sudo apt install -y git
   echo "Git installation completed."
+  mkdir -p ~/Dotfiles
+  git clone --depth=1 https://github.com/lrivas3/Dotfiles-Ubuntu.git ~/Dotfiles/Dotfiles-Ubuntu
 fi
 
 BASE_DIR=~/Dotfiles/Dotfiles-Ubuntu
 
-mkdir -p ~/.local/share/fonts/ && FONTS_DIR=~/.local/share/fonts/
+mkdir -p ~/.local/share/fonts/ && FONTS_DIR=~/.local/share/fonts
 
 echo "Copying config files to desired destination"
 echo "Copying zsh files"
@@ -29,7 +31,7 @@ ln -s $BASE_DIR/.gitconfig ~/
 
 
 echo "installing packages with apt"
-sudo apt install zsh curl ranger kitty neofetch cava fzf lsd bat nvm python3 python3-pip nala
+sudo apt install zsh curl ranger kitty neofetch cava fzf lsd bat python3 python3-pip nala
 echo "Installing nvim"
 sudo snap install nvim --classic
 echo "Installing config files for nvim"
@@ -43,6 +45,23 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo "Installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-nvim
+echo "Installing zsh plugins"
+sudo mkdir -p /usr/share/zsh/plugins && cd /usr/share/zsh/plugins/
+# z
+sudo git clone https://github.com/agkozak/zsh-z.git
+# sudo
+sudo mkdir -p zsh-sudo && sudo wget -P ./zsh-sudo https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh 
+# you should use
+sudo git clone https://github.com/MichaelAquilina/zsh-you-should-use.git
+# autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git
+# highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+# zsh-history-substring-search
+sudo git clone https://github.com/zsh-users/zsh-history-substring-search.git
+
+cd
+
+source ~/.zshrc
 
 echo "DONE!!"
